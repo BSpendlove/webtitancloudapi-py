f496f13634ea9fe41657102c2a1f5b01cf4ae1b8
Brandon Spendlove <brandon.spendlove@unitednet.co.uk>
2019-02-21 16:21:32 +0000

    defaultPolicy API Added

--- webtitan/webtitancloudapi/controllers/defaultPolicy_api.py
+++ webtitan/webtitancloudapi/controllers/defaultPolicy_api.py
@@ -3,7 +3,7 @@ import requests
 
 class DefaultPolicyAPI(object):
     jsonmap = {
-        'id':'categoryid',
+        'categoryid':'categoryid',
         'offset':'offset',
         'limit':'limit',
         'allowed':'allowed',
@@ -19,8 +19,44 @@ class DefaultPolicyAPI(object):
         else:
             self.webtitan = webtitanAPI()
 
+    def get_default_specific_filtercategory(self, categoryid):
+        #http://apidoc.webtitancloud.com/#api-Default_Policy-GetRestapiDefaultpolicyCategoriesCategoryid
+        apiUrl = '/defaultpolicy/categories/%s' %(categoryid)
+        req = self.webtitan.query(apiUrl, method=requests.get)
+        return(req)
+
     def get_default_filtering_policy(self):
         #http://apidoc.webtitancloud.com/#api-Default_Policy-GetRestapiDefaultpolicy
         apiUrl = '/defaultpolicy'
         req = self.webtitan.query(apiUrl, method=requests.get)
+        return(req)
+
+    def get_default_filtering_policy_categories(self):
+        #http://apidoc.webtitancloud.com/#api-Default_Policy-GetRestapiDefaultpolicyCategories
+        apiUrl = '/defaultpolicy/categories'
+        req = self.webtitan.query(apiUrl, method=requests.get)
+        return(req)
+
+    def update_default_specific_filtercategory(self, categoryid, **kwargs):
+        #http://apidoc.webtitancloud.com/#api-Default_Policy-PostRestapiCategoriesCategoryid
+        payload = {'id' : categoryid}
+        payload.update(self.webtitan.payloadBuilder(self.jsonmap, **kwargs))
+        apiUrl = '/categories/%s' %(categoryid)
+        req = self.webtitan.query(apiUrl, method=requests.post, data=payload, json_handle=False)
+        return(req)
+
+    def update_default_filtering_policy(self, **kwargs):
+        #http://apidoc.webtitancloud.com/#api-Default_Policy-PostRestapiDefaultpolicy
+        payload = {}
+        payload.update(self.webtitan.payloadBuilder(self.jsonmap, **kwargs))
+        apiUrl = '/defaultpolicy'
+        req = self.webtitan.query(apiUrl, method=requests.post, data=payload, json_handle=False)
+        return(req)
+
+    def update_all_default_filtercategories(self, **kwargs):
+        #http://apidoc.webtitancloud.com/#api-Default_Policy-PostRestapiDefaultpolicyCategories
+        payload = {}
+        payload.update(self.webtitan.payloadBuilder(self.jsonmap, **kwargs))
+        apiUrl = '/defaultpolicy/categories'
+        req = self.webtitan.query(apiUrl, method=requests.post, data=payload, json_handle=False)
         return(req)
